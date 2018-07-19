9cdeb4628737c37ff47efcf94cf01cc80b0c2f96
Raj Kumar <raj@apptology.in>
2018-07-19 15:23:41 +0530

    Initial Commit

---
+++ app/src/main/java/com/e/viewpagersinglefragmentkotlin/Movie.kt
@@ -0,0 +1,5 @@
+package com.e.viewpagersinglefragmentkotlin
+
+
+// Get the list of movies from the JSON file
+data class Movie(val title: String, val rating: Int, val posterUri: String, val overview: String)
